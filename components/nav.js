fa5c9b174298563b0c3427b964952bd6b32f3def
Rached <bch.rached@gmail.com>
2019-12-19 15:39:32 +0100

    adding simple styles under jsx

--- components/nav.js
+++ components/nav.js
@@ -15,6 +15,21 @@ const Nav = () => {
           </Link>
         </li>
       </ul>
+      <style jsx>{`
+        ul {
+          list-style: none;
+        }
+        li {
+          display: inline-block;
+        }
+        .nav-item {
+          background-color: black;
+          color: white;
+          padding: 10px;
+          margin: 5px;
+          text-decoration: none;
+        }
+      `}</style>
     </div>
   );
 };
