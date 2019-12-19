6f848329619718e1d8d759e107ad06934ff36967
Rached <bch.rached@gmail.com>
2019-12-19 17:02:05 +0100

    List books using isomorphic-unfetch

--- components/nav.js
+++ components/nav.js
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
         Navbar
       </a>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
             <Link href="/">
               <a className="nav-link">Home</a>
             </Link>
           </li>
-          <li class="nav-item">
+          <li className="nav-item">
             <Link href="/about">
               <a className="nav-link">About</a>
             </Link>
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,41 @@
 import Layout from "../components/layout";
 import Head from "next/head";
+import fetch from "isomorphic-unfetch";
 
-const Index = () => (
-  <Layout>
-    <Head>
-      <title>Home page</title>
-      <meta name="description" content="Description of the home page..." />
-    </Head>
-    <h1>Home page</h1>
-    <p>Hello world</p>
-  </Layout>
-);
+const Index = props => {
+  const books = props.books;
+
+  return (
+    <Layout>
+      <Head>
+        <title>Home page</title>
+        <meta name="description" content="Description of the home page..." />
+      </Head>
+      <h1>Home page</h1>
+      <h2>Book list</h2>
+      {books.length &&
+        books.map(book => {
+          //console.log({ book });
+          return (
+            <div className="card" key={book.ID}>
+              <div className="card-body">
+                <h5 className="card-title">{book.Title}</h5>
+                <p className="card-text">{book.Description}</p>
+              </div>
+            </div>
+          );
+        })}
+    </Layout>
+  );
+};
+
+Index.getInitialProps = async context => {
+  console.log({ context });
+  const result = await fetch(`https://fakerestapi.azurewebsites.net/api/Books`);
+  const books = await result.json();
+  return {
+    books
+  };
+};
 
 export default Index;
