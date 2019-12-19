0309d8f8c87a1515fb5ec305d4a202c3b85c5f36
Rached <bch.rached@gmail.com>
2019-12-19 16:11:02 +0100

    Using Head component

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,12 @@
 import Layout from "../components/layout";
+import Head from "next/head";
 
 const About = () => (
   <Layout>
+    <Head>
+      <title>About page</title>
+      <meta name="description" content="Description of the about page..." />
+    </Head>
     <h1>About page</h1>
     <p>Hello about</p>
   </Layout>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Layout from "../components/layout";
+import Head from "next/head";
 
 const Index = () => (
   <Layout>
+    <Head>
+      <title>Home page</title>
+      <meta name="description" content="Description of the home page..." />
+    </Head>
     <h1>Home page</h1>
     <p>Hello world</p>
   </Layout>
