14510f2b87609d5336b1a6951c0fe311d82e43e0
Rached <bch.rached@gmail.com>
2019-12-19 15:24:07 +0100

    Basic creation of pages

---
+++ pages/about.js
@@ -0,0 +1,4 @@
+const About = () => {
+  return <h1>About page</h1>;
+};
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+const Index = () => (
+  <div>
+    <h1>Page d'accueil</h1>
+    <p>Hello Next.js</p>
+  </div>
+);
+
+export default Index;
