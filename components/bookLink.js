817f596bedf76c40777ff56372126da959ba1b70
Rached <bch.rached@gmail.com>
2019-12-20 15:51:54 +0100

    Clean link book details page without node endpoint

--- components/bookLink.js
+++ components/bookLink.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import slugify from "slugify";
 
 const BookLink = ({ title, id }) => {
+  const sluggedTitle = slugify(title);
   return (
-    <Link href={`book?id=${id}`}>
+    <Link as={`/book-details/${sluggedTitle}-${id}`} href={`book?id=${id}`}>
       <a>{title}</a>
     </Link>
   );
