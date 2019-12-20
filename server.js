31f53701aa8fe3e15660a0e08082904d1a24985f
Rached <bch.rached@gmail.com>
2019-12-20 16:22:54 +0100

    Create node endpoint for new clean url book details

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
 
-//const express = require("express");
+const express = require("express");
 
 const port = 3000;
 const dev = process.env.NODE_ENV !== "production";
@@ -11,22 +10,20 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
-    const parsedUrl = parse(req.url, true);
-    const { pathname, query } = parsedUrl;
+  const server = express();
 
-    // if (pathname === "/a") {
-    //   app.render(req, res, "/b", query);
-    // } else if (pathname === "/b") {
-    //   app.render(req, res, "/a", query);
-    // } else {
-    //   handle(req, res, parsedUrl);
-    // }
-    handle(req, res, parsedUrl);
-  }).listen(port, err => {
+  server.get("/book-details/:slug", function(req, res) {
+    const bookId = req.params.slug.split("-").pop();
+    const queryParams = { id: bookId };
+    app.render(req, res, "/book", queryParams);
+  });
+
+  server.get("*", function(req, res) {
+    return handle(req, res);
+  });
+
+  server.listen(port, err => {
     if (err) throw err;
-    console.log("> Ready on http://localhost:3000");
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
